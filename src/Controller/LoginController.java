83136e2ad4ea794f062c3c6eafaad389155a0755
Matheus <joaosobramatheus@gmail.com>
2025-11-08 19:30:48 -0300

    <feat> Adiciona construção de email e senha no login

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,6 +1,20 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginController {
 
-}
+    @FXML
+    private TextField emailDoLogin;
 
+    @FXML
+    private TextField senhaDoLogin;
+
+    @FXML
+    void fazerLogin(ActionEvent event) {
+
+    }
+
+}
