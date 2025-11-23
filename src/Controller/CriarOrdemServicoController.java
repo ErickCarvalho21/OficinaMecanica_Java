24eaea6896c4cf6cfb742c20dd9d8849adc525b1
Matheus <joaosobramatheus@gmail.com>
2025-11-23 12:24:45 -0300

    <feat> Adiciona no CriarOrdemServicoController, o initialize que é o método que vem primeiro, Combo de clientes e peças

--- src/Controller/CriarOrdemServicoController.java
+++ src/Controller/CriarOrdemServicoController.java
@@ -1,20 +1,19 @@
 package Controller;
 
+import DB.ClienteDAO;
+import DB.PecaDAO;
 import Model.*;
 import Templates.Alertas;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.net.URL;
 import java.text.DecimalFormat;
+import java.util.ResourceBundle;
 
 public class CriarOrdemServicoController {
 
@@ -66,6 +65,44 @@ public class CriarOrdemServicoController {
     private ObservableList<ItemPeca> pecasAdicionadas = FXCollections.observableArrayList();
     private Alertas alertas = new Alertas();
     private DecimalFormat df = new DecimalFormat("R$ #,##0.00");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        comboCliente.setItems(ClienteDAO.listarClientes());
+        configurarComboCliente();
+
+
+        // comboPeca.setItems(PecaDAO.listarPecas());
+
+        comboStatus.setItems(FXCollections.observableArrayList(
+                "Em Serviço", "Aguardando Peças", "Pronto para Entrega", "Finalizado"
+        ));
+        comboStatus.setValue("Em Serviço");
+
+        colPecaNome.setCellValueFactory(data -> data.getValue().nomePecaProperty());
+        colPecaQtd.setCellValueFactory(data -> data.getValue().quantidadeProperty());
+        colPecaPreco.setCellValueFactory(data -> data.getValue().precoUnitarioProperty());
+        colPecaTotal.setCellValueFactory(data -> data.getValue().totalProperty());
+        tabelaPecas.setItems(pecasAdicionadas);
+    }
+
+    private void configurarComboCliente() {
+        comboCliente.setButtonCell(new ListCell<Cliente>() {
+            @Override
+            protected void updateItem(Cliente item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty || item == null ? null : item.getNome());
+            }
+        });
+        comboCliente.setCellFactory(lv -> new ListCell<Cliente>() {
+            @Override
+            protected void updateItem(Cliente item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty || item == null ? null : item.getNome() + " - CPF: " + item.getCpf());
+            }
+        });
+    }
 
     @FXML
     void adicionarPeca(ActionEvent event) {
