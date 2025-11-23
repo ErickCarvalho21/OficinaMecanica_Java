0b018b4d141ccbc1d02e9e0c66503944a2681100
Matheus <joaosobramatheus@gmail.com>
2025-11-23 19:29:52 -0300

    <feat and style> Adiciona visual em peças e controller do mesmo

---
+++ src/Controller/PecasController.java
@@ -0,0 +1,87 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class PecasController {
+
+    @FXML
+    private TableColumn<?, ?> colEstoque;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colNome;
+
+    @FXML
+    private TableColumn<?, ?> colPreco;
+
+    @FXML
+    private Label lblAlertas;
+
+    @FXML
+    private Label lblTotal;
+
+    @FXML
+    private Label lblUsuario;
+
+    @FXML
+    private Label lblValorEstoque;
+
+    @FXML
+    private TableView<?> tabelaPecas;
+
+    @FXML
+    private TextField txtBusca;
+
+    @FXML
+    void ajustarEstoque(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void excluirPeca(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irParaAdicionar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irParaEditar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irParaOrdens(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irParaVeiculos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limparBusca(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarParaPainel(ActionEvent event) {
+
+    }
+
+}
