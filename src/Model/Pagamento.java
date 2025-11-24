8c6a487ec93878fc21572ec69bd7e5cacac58b84
Matheus <joaosobramatheus@gmail.com>
2025-11-23 22:25:34 -0300

    <feat> Adiciona Pagamento no model

--- src/Model/Pagamento.java
+++ src/Model/Pagamento.java
@@ -1,64 +1,34 @@
 package Model;
-import java.util.Date;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 
 public class Pagamento {
-    private int idPagamento;
-    private int idOrdemDeServico;
-    private double valor;
-    private String status;
-    private String formaDePagamento;
-    private Date dataDoAno;
+    private SimpleStringProperty idPagamento;
+    private SimpleStringProperty idOrdem;
+    private SimpleDoubleProperty valor;
+    private SimpleStringProperty formaPagamento;
+    private SimpleStringProperty status;
+    private SimpleStringProperty dataPagamento;
 
-    public void registrarPagamento(){throw new UnsupportedOperationException("TODO");}
-    public void estornarPagamento(){throw new UnsupportedOperationException("TODO");}
-    public void consultarStatus(){throw new UnsupportedOperationException("TODO");}
-    
-    public Pagamento(int idPagamento, Date dataDoAno, double valor, String status, String formaDePagamento,
-            int idOrdemDeServico) {
-        this.idPagamento = idPagamento;
-        this.dataDoAno = dataDoAno;
-        this.valor = valor;
-        this.status = status;
-        this.formaDePagamento = formaDePagamento;
-        this.idOrdemDeServico = idOrdemDeServico;
-    }
-    public int getIdPagamento() {
-        return idPagamento;
-    }
-    public void setIdPagamento(int idPagamento) {
-        this.idPagamento = idPagamento;
-    }
-    public Date getDataDoAno() {
-        return dataDoAno;
-    }
-    public void setDataDoAno(Date dataDoAno) {
-        this.dataDoAno = dataDoAno;
-    }
-    public double getValor() {
-        return valor;
-    }
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-    public String getStatus() {
-        return this.status;
-    }
-    public void setStatus(String status) {
-        this.status = status;
-    }
-    public String getFormaDePagamento() {
-        return formaDePagamento;
-    }
-    public void setFormaDePagamento(String formaDePagamento) {
-        this.formaDePagamento = formaDePagamento;
-    }
-    public int getIdOrdemDeServico() {
-        return idOrdemDeServico;
-    }
-    public void setIdOrdemDeServico(int idOrdemDeServico) {
-        this.idOrdemDeServico = idOrdemDeServico;
+    public Pagamento(String idPagamento, String idOrdem, double valor, String formaPagamento, String status, String dataPagamento) {
+        this.idPagamento = new SimpleStringProperty(idPagamento);
+        this.idOrdem = new SimpleStringProperty(idOrdem);
+        this.valor = new SimpleDoubleProperty(valor);
+        this.formaPagamento = new SimpleStringProperty(formaPagamento);
+        this.status = new SimpleStringProperty(status);
+        this.dataPagamento = new SimpleStringProperty(dataPagamento);
     }
 
-    
-    
+
+    public String getIdPagamento() { return idPagamento.get(); }
+    public String getIdOrdem() { return idOrdem.get(); }
+    public double getValor() { return valor.get(); }
+    public String getFormaPagamento() { return formaPagamento.get(); }
+    public String getStatus() { return status.get(); }
+    public String getDataPagamento() { return dataPagamento.get(); }
+
+    public SimpleStringProperty formaPagamentoProperty() { return formaPagamento; }
+    public SimpleDoubleProperty valorProperty() { return valor; }
+    public SimpleStringProperty statusProperty() { return status; }
 }
